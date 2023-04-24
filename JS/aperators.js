8ea8d29e0ce11b9789be3b3167a0fa951e1316b3
Viurikasmith <129077024+Viurikasmith@users.noreply.github.com>
2023-04-24 09:27:12 -0500

    main

--- JS/aperators.js
+++ JS/aperators.js
@@ -1,2 +1,9 @@
- var Document
- 
+ // conversion larga :
+ var celcius1 =promt("introduzca la temperatura en grados celcius: ") ;
+ celcius1 = parseFloat(celcius1);
+// coversion corta:
+//var celcius1 =parseFloat(promt
+("introduzca la temperatura en grados celcius:");
+//procedo a hacer la conversion de celcius a farenheit:
+var farenheit = 9/5*celcius1+32;
+document.write("de celcius a farenheit", farenheit1);
